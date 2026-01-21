9e7d48a7fceb66801385e4487e7a5dc19f927029
CaioRian187 <caiorian246810@gmail.com>
2026-01-21 08:28:02 -0300

    Criatando projeto

---
+++ src/main/java/com/CaioRian/AvaliacoesFisicas/AvaliacoesFisicasApplication.java
@@ -0,0 +1,13 @@
+package com.CaioRian.AvaliacoesFisicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacoesFisicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacoesFisicasApplication.class, args);
+	}
+
+}
