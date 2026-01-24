07415a42265c397d9a398318f3a845a8aa87d053
CaioRian187 <caiorian246810@gmail.com>
2026-01-24 00:49:46 -0300

    Add ControllerNavegacao

---
+++ src/main/java/com/CaioRian/AvaliacoesFisicas/controller/ControllerNavegacao.java
@@ -0,0 +1,22 @@
+package com.CaioRian.AvaliacoesFisicas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControllerNavegacao {
+    @GetMapping("/aluno")
+    public String paginaAluno() {
+        return "aluno"; // Aqui deve ser o nome exato do arquivo .html sem a extensão
+    }
+
+    @GetMapping("/antropometria")
+    public String paginaAntropometria() {
+        return "antropometria";
+    }
+
+    @GetMapping("/dobras-cutaneas")
+    public String paginaDobras() {
+        return "dobras";
+    }
+}
